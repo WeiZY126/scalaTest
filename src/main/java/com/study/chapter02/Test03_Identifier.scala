ee62b198e276dade19d7e8452ca1611a0ecfd998
wzy <1369150463@qq.com>
2021-10-08 16:45:09 +0800

    2021/10/8

---
+++ src/main/java/com/study/chapter02/Test03_Identifier.scala
@@ -0,0 +1,14 @@
+package com.study.chapter02
+
+object Test03_Identifier {
+  def main(args:Array[String]): Unit ={
+    val hello: String = "";
+    val _abc = 111;
+
+    //以操作头开头，只包含操作符
+    val +-*/ = "hello";
+    println(+-*/);
+
+
+  }
+}
