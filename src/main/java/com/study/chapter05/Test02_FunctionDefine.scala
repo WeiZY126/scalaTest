9e837dbf9bcbd9b4bf549f971f3fc7f59d9b706d
wzy <1369150463@qq.com>
2021-11-29 11:43:25 +0800

    2021/11/26

---
+++ src/main/java/com/study/chapter05/Test02_FunctionDefine.scala
@@ -0,0 +1,12 @@
+package com.study.chapter05
+
+object Test02_FunctionDefine {
+  def main(args: Array[String]): Unit = {
+    //1.无参，无返回值
+    def f1()={
+      println("1.无参，无返回值")
+    }
+    f1()
+  }
+
+}
